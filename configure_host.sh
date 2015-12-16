9c3e87259619d92c53f7541b1a14c02b9bc4b79a
pat <pat@22acacia.com>
2015-12-16 00:40:46 -0800

    there is no -o flag, that's a copy/pasta curl error

--- configure_host.sh
+++ configure_host.sh
@@ -6,11 +6,11 @@ echo "ensure gsutil is installed"
 /opt/google-cloud-sdk/bin/gcloud components install gsutil
 
 echo "download googlecli provider"
-/opt/google-cloud-sdk/bin/gsutil cp gs://build-artifacts-public-eu/terraform-provider-googlecli -o /usr/local/bin/terraform-provider-googlecli
+/opt/google-cloud-sdk/bin/gsutil cp gs://build-artifacts-public-eu/terraform-provider-googlecli /usr/local/bin/terraform-provider-googlecli
 chmod +x /usr/local/bin/terraform-provider-googlecli
 
 echo "download sossity and deployable jars"
-/opt/google-cloud-sdk/bin/gsutil cp gs://build-artifacts-public-eu/*.jar -o /usr/local/lib/
+/opt/google-cloud-sdk/bin/gsutil cp gs://build-artifacts-public-eu/*.jar /usr/local/lib/
 
 echo "ensure kubectl is installed and that dataflow commands for gcloud are installed"
 which kubectl
