2efd2afd69037d312366f9865cb06b6d137c349c
lusciouspear <bradfordstephens@gmail.com>
2016-01-13 19:05:49 -0800

    added cluster machine_type

--- tinyconfig.clj
+++ tinyconfig.clj
@@ -1,4 +1,9 @@
-{:cluster   {:name "hxhstack" :initial_node_count 3 :master_auth {:username "hx" :password "hstack"}}
+{:cluster   {:name "hxhstack" :initial_node_count 4 :master_auth {:username "hx" :password "hstack"} :node_config {:oauth_scopes ["https://www.googleapis.com/auth/compute"
+                                                                                                                                  "https://www.googleapis.com/auth/devstorage.read_only"
+                                                                                                                                  "https://www.googleapis.com/auth/logging.write"
+                                                                                                                                  "https://www.googleapis.com/auth/monitoring"
+                                                                                                                                  "https://www.googleapis.com/auth/cloud-platform"]
+                                                                                                                   :machine_type "n1-standard-4"}}
  :opts      {:composer-classpath    ["/usr/local/lib/angleddream-bundled.jar"] ;where all the jar files live. no trailing slash. may be overriden by env var in production? also be sure to build thick jars from angled-dream for deps
              :maxNumWorkers "1" :numWorkers "1" :zone "europe-west1-c" :workerMachineType "n1-standard-1"
              :stagingLocation "gs://hx-test/staging-eu"}
@@ -12,9 +17,7 @@
              "orionresponsys"
              {:transform-graph ["/usr/local/lib/orion-responsys.jar"]}
              "orionresponsysmailer"
-             {:transform-graph ["/usr/local/lib/orion-responsys-mailer.jar"]}
-
-             }
+             {:transform-graph ["/usr/local/lib/orion-responsys-mailer.jar"]}}
  :sources   {"stream1bts" {:type "kub"}
              "orion" {:type "kub"}}
  :sinks     {"sink1bts" {:type "gcs" :bucket "sink1-bts-test-two"}
