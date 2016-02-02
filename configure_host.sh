0970d6586e1b5d1596bfa20599a1033fd1d56014
pat <pat@22acacia.com>
2016-02-01 23:04:03 -0800

    output of version-parser now driving jar downloads
    added some commments and set -xe on the bash scripts

--- configure_host.sh
+++ configure_host.sh
@@ -1,39 +1,43 @@
+set -ex
+
 cwd=`pwd`
 
 echo "download and install terraform and custom provider"
-curl https://releases.hashicorp.com/terraform/0.6.9/terraform_0.6.9_linux_amd64.zip -o $HOME/$CIRCLE_PROJECT_REPONAME/terraform.zip
-sudo unzip $HOME/$CIRCLE_PROJECT_REPONAME/terraform.zip -d /usr/local/bin/
+#curl https://releases.hashicorp.com/terraform/0.6.9/terraform_0.6.9_linux_amd64.zip -o $HOME/$CIRCLE_PROJECT_REPONAME/terraform.zip
+#sudo unzip $HOME/$CIRCLE_PROJECT_REPONAME/terraform.zip -d /usr/local/bin/
 
 echo "ensure gsutil is installed"
-sudo /opt/google-cloud-sdk/bin/gcloud components install gsutil
+#sudo /opt/google-cloud-sdk/bin/gcloud components install gsutil
 
 echo "create the account file"
-echo $GOOGLE_CREDENTIALS > $HOME/$CIRCLE_PROJECT_REPONAME/account.json
-export GOOGLE_APPLICATION_CREDENTIALS=$HOME/$CIRCLE_PROJECT_REPONAME/account.json
+#echo $GOOGLE_CREDENTIALS > $HOME/$CIRCLE_PROJECT_REPONAME/account.json
+#export GOOGLE_APPLICATION_CREDENTIALS=$HOME/$CIRCLE_PROJECT_REPONAME/account.json
 
 echo "auth the local sudo gcloud"
-sudo /opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file $HOME/$CIRCLE_PROJECT_REPONAME/account.json
+#sudo /opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file $HOME/$CIRCLE_PROJECT_REPONAME/account.json
 
 echo "download googlebigquery provider"
-sudo /opt/google-cloud-sdk/bin/gsutil cp gs://${GSTORAGE_DEST_BUCKET}/terraform-provider-googlebigquery /usr/local/bin/terraform-provider-googlebigquery
-sudo chmod +x /usr/local/bin/terraform-provider-googlebigquery
+#sudo /opt/google-cloud-sdk/bin/gsutil cp gs://${GSTORAGE_DEST_BUCKET}/terraform-provider-googlebigquery /usr/local/bin/terraform-provider-googlebigquery
+#sudo chmod +x /usr/local/bin/terraform-provider-googlebigquery
 
 echo "download googleappengine provider"
-sudo /opt/google-cloud-sdk/bin/gsutil cp gs://${GSTORAGE_DEST_BUCKET}/terraform-provider-googleappengine /usr/local/bin/terraform-provider-googleappengine
-sudo chmod +x /usr/local/bin/terraform-provider-googleappengine
+#sudo /opt/google-cloud-sdk/bin/gsutil cp gs://${GSTORAGE_DEST_BUCKET}/terraform-provider-googleappengine /usr/local/bin/terraform-provider-googleappengine
+#sudo chmod +x /usr/local/bin/terraform-provider-googleappengine
 
 echo "download googlecli provider"
-sudo /opt/google-cloud-sdk/bin/gsutil cp gs://${GSTORAGE_DEST_BUCKET}/terraform-provider-googlecli /usr/local/bin/terraform-provider-googlecli
-sudo chmod +x /usr/local/bin/terraform-provider-googlecli
+#sudo /opt/google-cloud-sdk/bin/gsutil cp gs://${GSTORAGE_DEST_BUCKET}/terraform-provider-googlecli /usr/local/bin/terraform-provider-googlecli
+#sudo chmod +x /usr/local/bin/terraform-provider-googlecli
+
+echo "create specified jars list"
+cd version-parser
+lein run
 
 echo "download specified jars"
 while read jar_spec; do
   jar_spec_arr=($jar_spec)
-  sudo /opt/google-cloud-sdk/bin/gsutil cp gs://${GSTORAGE_DEST_BUCKET}/${jar_spec_arr[0]}/VERSIONS.txt ${jar_spec_arr[0]}.versions.txt
-  jar_name=`grep ${jar_spec_arr[1]} ${jar_spec_arr[0]}.versions.txt | tr -d '\n'`
-  sudo /opt/google-cloud-sdk/bin/gsutil cp gs://${GSTORAGE_DEST_BUCKET}/${jar_spec_arr[0]}/${jar_name} /usr/local/lib/${jar_spec_arr[2]}
-done < ${cwd}/jar-specs.txt
-
+  sudo /opt/google-cloud-sdk/bin/gsutil cp gs://${GSTORAGE_DEST_BUCKET}/${jar_spec_arr[0]} /usr/local/lib/${jar_spec_arr[1]}
+done < ${cwd}/jar-versions
+exit 0
 echo "ensure kubectl is installed and that dataflow commands for gcloud are installed"
 which kubectl
 if [ $? -eq 1 ]; then
--- version-parser/src/version_parser/core.clj
+++ version-parser/src/version_parser/core.clj
@@ -3,6 +3,7 @@
   (:require [clojure.java.io :as io])
   (:import [java.io PushbackReader]))
 
+;; read config into map
 (def conf (with-open [r (clojure.java.io/reader "../tinyconfig.clj")] 
   (read (PushbackReader. r))))
 
@@ -10,6 +11,10 @@
   [& args]
   (def pipelines (:pipelines conf))
 
+  ; clear out the file before writing to it
+  (spit "../jar-versions" "")
+
+  ; for each pipeline, create <key>/jarname jarname pairs and write to file
   (doseq [[k v]pipelines] 
     (spit "../jar-versions" 
       (clojure.string/join " " 
