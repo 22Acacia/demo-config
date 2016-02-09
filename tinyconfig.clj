db2234f2c0c405607b15fa7922b88c086a625655
lusciouspear <bradfordstephens@gmail.com>
2016-02-08 23:44:44 -0800

    added 2 new settings

--- tinyconfig.clj
+++ tinyconfig.clj
@@ -2,6 +2,8 @@
              :remote-libs-path          "/usr/local/lib"
              :sink-resource-version "1"
              :source-resource-version "1"
+             :appengine-gstoragekey "hxtest-1.0-SNAPSHOT"
+             :default-sink-docker-image "gcr.io/hx-test/store-sink"
              :error-buckets             false
              :system-jar-info {:angleddream {:name "angleddream-bundled-0.1-ALPHA.jar"
                                              :pail "build-artifacts-public-eu"
@@ -9,16 +11,18 @@
                                :sossity     {:name "sossity-0.1.0-SNAPSHOT-standalone.jar"
                                              :pail "build-artifacts-public-eu"
                                              :key  "sossity"}}}
- :cluster   {:name        "hxhstack" :initial_node_count 4 :master_auth {:username "hx" :password "hstack"}
+ :cluster   {:name        "hxhstack" :initial_node_count 4
+             :master_auth {:username "hx" :password "hstack"}
              :node_config {:oauth_scopes ["https://www.googleapis.com/auth/compute"
                                           "https://www.googleapis.com/auth/devstorage.read_only"
                                           "https://www.googleapis.com/auth/logging.write"
                                           "https://www.googleapis.com/auth/monitoring"
                                           "https://www.googleapis.com/auth/cloud-platform"]
                            :machine_type "n1-standard-1"}}
- :opts      {:maxNumWorkers   "1" :numWorkers "1" :zone "europe-west1-c" :workerMachineType "n1-standard-1"
-             :stagingLocation "gs://hx-test/staging-eu"}
- :provider  {:credentials "${file(\"/home/ubuntu/demo-config/account.json\")}" :project "hx-test"}
+ :opts      {:maxNumWorkers   "1" :numWorkers "1" :zone "europe-west1-c"
+             :workerMachineType "n1-standard-1"   :stagingLocation "gs://hx-test/staging-eu"}
+ :provider  {:credentials "${file(\"/home/ubuntu/demo-config/account.json\")}"
+             :project "hx-test"}
  :pipelines {"testpipeline"
              {:transform-jar "identitypipeline-0.1-ALPHA.jar"
               :pail "build-artifacts-public-eu"
@@ -31,11 +35,14 @@
              {:transform-jar "orion-transform-0.1-ALPHA.jar"
               :pail "build-artifacts-public-eu"
               :key "orion-transform"}}
- :sources   {"testendpoint" {:type "kub"}
-             "orion"      {:type "kub"}}
- :sinks     {"testsink"                 {:type "gcs" :bucket "testsink-bucket"}
-             "orionsink"                {:type "gcs" :bucket "orionsinkbucket"}
-             "orionbq"                  {:type "bq" :bigQueryDataset "hx_orion_staging" :bigQueryTable "hx_orion" :bigQuerySchema "/home/ubuntu/demo-config/orion.json"}}
+ :sources   {"testendpoint" {:type "gae"}
+             "orion"        {:type "gae"}}
+ :sinks     {"testsink"     {:type "gcs" :bucket "testsink-bucket"}
+             "orionsink"    {:type "gcs" :bucket "orionsinkbucket"}
+             "orionbq"      {:type "bq"
+                             :bigQueryDataset "hx_orion_staging"
+                             :bigQueryTable "hx_orion"
+                             :bigQuerySchema "/home/ubuntu/demo-config/orion.json"}}
  :edges     [{:origin "testendpoint" :targets ["testpipeline"]}
              {:origin "testpipeline" :targets ["testsink"]}
              {:origin "orion" :targets ["orionbqfilter" "orionidentitypipe"]}
